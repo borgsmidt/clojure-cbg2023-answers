6c3a692441489a97a0a0d4ac22422da705ba8365
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2023-02-05 23:38:12 +0100

    clean up

--- src/clojure_cbg2023_answers/lecture-20-answer.clj
+++ src/clojure_cbg2023_answers/lecture-20-answer.clj
@@ -4,14 +4,14 @@
 ; particularly functional approach). Instead it returns the result as a value,
 ; which greatly improves testability.
 
+(defn assert [test] (if (false? test) (throw (AssertionError. "test failed"))))
+
 (def pet-type-to-human-age {:dog 7 :cat 5 :fish 10})
 
 (defn human-age-of-pet
   "Returns age of the specified pet in human years"
   [pet-type age]
   (* (get pet-type-to-human-age pet-type) age))
-
-(defn assert [test] (if (false? test) (throw (AssertionError. "test failed"))))
 
 (assert (= 0 (human-age-of-pet :dog 0)))
 (assert (= 35 (human-age-of-pet :dog 5)))
--- src/clojure_cbg2023_answers/lecture-29-answer.clj
+++ src/clojure_cbg2023_answers/lecture-29-answer.clj
@@ -6,19 +6,19 @@
 
 (defn assert [test] (if (false? test) (throw (AssertionError. "test failed"))))
 
-(def car-type-prices {:bmw 60000 :ferrari 100000 :fiat 20000})
+(def car-prices {:bmw 60000 :ferrari 100000 :fiat 20000})
 
 (def coupon-discount-pct 20)
 (def valid-coupon-code "my-valid-coupon")
 
 (defn analyze-budget
-  "Returns an analysis of the supplied budget and coupon code"
+  "Returns an analysis of the specified budget and coupon code"
   [budget code]
   (let
     [coupon-valid (= code valid-coupon-code)
      factor (if coupon-valid (- 1 (/ coupon-discount-pct 100)) 1)
-     adjusted (map (fn [[type price]] [type (* price factor)]) car-type-prices)
-     affordable (filter (fn [[type price]] (<= price budget)) adjusted)]
+     adjusted (map (fn [[car price]] [car (* price factor)]) car-prices)
+     affordable (filter (fn [[car price]] (<= price budget)) adjusted)]
     {:coupon (if coupon-valid "The coupon is valid" "The coupon is invalid")
      :cars (into {} affordable)}))
 
